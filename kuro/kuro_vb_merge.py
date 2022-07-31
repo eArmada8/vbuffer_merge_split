b14bd4dfaaf914f60d088b81c4b974bfcd75d8d9
eArmada8 <103985972+eArmada8@users.noreply.github.com>
2022-07-31 16:40:33 -0700

    Update kuro_vb_merge.py

--- kuro/kuro_vb_merge.py
+++ kuro/kuro_vb_merge.py
@@ -9,7 +9,7 @@ import glob, os, re, shutil
 def retrieve_indices():
     # Make a list of all vertex buffer indices in the current folder
     # NOTE: Will *not* include index buffers without vertex data! (i.e. ib files without corresponding vb files)
-    return sorted([re.findall('^\d+', x)[0] for x in glob.glob('*-vb0*txt')])
+    return sorted([re.findall('^\d+', x)[0] for x in glob.glob('*-vb7*txt')])
 
 def copy_ib_file_to_output(fileindex):
     # Copy the index buffer file to the output directory unmodified, if it exists
